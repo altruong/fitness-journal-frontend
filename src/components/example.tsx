e708d185b64a13192f4aab4e7e126112fe8ecfc6
Alan Truong <alan.truong2@gmail.com>
2020-10-08 02:08:32 +1100

    created form for createPost

---
+++ src/components/example.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+interface exampleProps {}
+
+export const example: React.FC<exampleProps> = ({}) => {
+  return <></>;
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,41 @@
-const Index = () => <div> Hello world</div>;
+import { Formik, Form, Field } from 'formik';
+
+interface Values {
+  title: string;
+  text: string;
+}
+
+const createPost = () => {
+
+}
+
+const Index = () => (
+  <>
+    <div> Hello world</div>
+    <Formik 
+    initialValues={{ title: '', text: '' }
+    onSubmit={async (values: Values) => {
+      createPost();
+    }}>
+      <Form>
+        <label htmlFor='title'>First Name</label>
+        <Field id='title' name='title' placeholder='insert title' />
+
+        <label htmlFor='text'>Last Name</label>
+        <Field id='text' name='text' placeholder='type text here' />
+
+        <label htmlFor='email'>Email</label>
+        <Field
+          id='email'
+          name='email'
+          placeholder='john@acme.com'
+          type='email'
+        />
+
+        <button type='submit'>Submit</button>
+      </Form>
+    </Formik>
+  </>
+);
 
 export default Index;
