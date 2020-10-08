36e0385b63876237d6027adaf31a4f95817d2963
Alan Truong <alan.truong2@gmail.com>
2020-10-09 03:53:01 +1100

    add: inputfield component

---
+++ src/components/InputField.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useField } from 'formik';
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+  Textarea,
+} from '@chakra-ui/core';
+
+interface InputFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  name: string;
+  textarea?: boolean;
+}
+
+export const InputField: React.FC<InputFieldProps> = ({
+  label,
+  size,
+  ...props
+}) => {
+  const [field, meta] = useField(props); // special hook from formik
+  return (
+    <FormControl isInvalid={!!meta.error}>
+      <FormLabel htmlFor={field.name}>{label}</FormLabel>
+      <Input {...field} {...props} id={field.name} />
+      {meta.error ? <FormErrorMessage>{meta.error}</FormErrorMessage> : null}
+    </FormControl>
+  );
+};
