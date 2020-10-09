00ef4bee72cf26878ee1b504e6e93fb6f2ed0ab2
Alan Truong <alan.truong2@gmail.com>
2020-10-09 17:04:10 +1100

    complete apolloclient initialistaion

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -5,6 +5,7 @@ import {
   NormalizedCacheObject,
   useApolloClient,
 } from '@apollo/client';
+import { concatPagination } from '@apollo/client/utilities';
 import { useMemo } from 'react';
 //  Normalized cache is the general cache used in Apollo
 let apolloClient: ApolloClient<NormalizedCacheObject>;
@@ -12,6 +13,9 @@ let apolloClient: ApolloClient<NormalizedCacheObject>;
 const createIsmorphicLink = () => {
   // Server side
   if (typeof window === 'undefined') {
+    const { SchemaLink } = require('@apollo/client/link/schema');
+    const { schema } = require('./schema');
+    return new SchemaLink({ schema });
   } else {
     // Client side
     const { HttpLink } = require('@apollo/client/link/http');
@@ -31,7 +35,15 @@ const createApolloClient = () => {
     link: createIsmorphicLink(), //Isomorphic meaning it will be running on both the client & server
     // cache will be an instance of InMemoryCache
     // i.e. where it stores the results of the queries
-    cache: new InMemoryCache(),
+    cache: new InMemoryCache({
+      typePolicies: {
+        Query: {
+          fields: {
+            allPosts: concatPagination().
+          }
+        }
+      }
+    }),
   });
 };
 
@@ -41,6 +53,20 @@ export const initializeApollo = (initialState = null) => {
   // Otherwise, go to the right side and create a new instance of the apolloClient
   // This is to determine if we are running on the client or server side rendering/generation
   const _apolloClient = apolloClient ?? createApolloClient();
+
+  // If your page has Next.js data fetching methods that use Apollo Client, the initial state
+  // gets hydrated here
+  if (initialState) {
+    _apolloClient.cache.restore(initialState);
+  }
+  // For SSG and SSR always create a new Apollo Client
+  // So two people's server request's cache never conflict
+  if (typeof window === 'undefined') {
+    return _apolloClient;
+  } else {
+    // Create Apollo Client once in the client
+    return apolloClient ?? _apolloClient;
+  }
 };
 
 // Return a copy of the apollo client
