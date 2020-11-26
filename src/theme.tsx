1d556343abef5299c2652c99f15dc70724a5c334
Alan Truong <alan.truong2@gmail.com>
2020-11-27 03:01:24 +1100

    update chakra theme

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,17 +1,41 @@
-import { theme as chakraTheme } from '@chakra-ui/react';
+import { extendTheme } from '@chakra-ui/react';
 
-const fonts = { ...chakraTheme.fonts, mono: `'Menlo', monospace` };
-
-const breakpoints = ['40em', '52em', '64em'];
-
-const theme = {
-  ...chakraTheme,
+// 2. Call `extendTheme` and pass your custom values
+const theme = extendTheme({
   colors: {
-    ...chakraTheme.colors,
-    black: '#16161D',
+    brand: {
+      100: '#f7fafc',
+      // ...
+      900: '#1a202c',
+    },
   },
-  fonts,
-  breakpoints,
-};
+  // components: {
+  //   Button: {
+  //     // 1. We can update the base styles
+  //     baseStyle: {
+  //       fontWeight: "bold", // Normally, it is "semibold"
+  //     },
+  //     // 2. We can add a new button size or extend existing
+  //     sizes: {
+  //       xl: {
+  //         h: "56px",
+  //         fontSize: "lg",
+  //         px: "32px",
+  //       },
+  //     },
+  //     // 3. We can add a new visual variant
+  //     variants: {
+  //       "with-shadow": {
+  //         bg: "red.400",
+  //         boxShadow: "0 0 2px 2px #efdfde",
+  //       },
+  //       // 4. We can override existing variants
+  //       solid: (props) => ({
+  //         bg: props.colorMode === "dark" ? "red.300" : "red.500",
+  //       }),
+  //     },
+  //   },
+  // },
+});
 
 export default theme;
